a14201f0c8d443c54c6e00f2c76fc7e6e78cdb02
igor-lacerda <igorlacerda5050@gmail.com>
2026-02-06 11:29:33 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+from utils import detect_objects_yolo_video, detect_objects_yolo_image, concatenate_videos
+from pathlib import Path
+
+def main():
+    # Exemplo
+    folder = r"C:\Users\Administrator\Desktop"
+    video = "exemplo.mp4"
+
+    # Parte 1
+    folder_path = Path(folder)
+    video_path = folder_path / "exemplo" / video
+
+    detect_objects_yolo_video(video_path, detect_person=True, detect_vehicles=True)
+
+    # Parte 2
+    video_path_2 = folder_path / "exemplo" / "yolo_recognition" / "exemplo_detected.mp4"
+
+    concatenate_videos(video_path, video_path_2, orientation='h')
+    concatenate_videos(video_path, video_path_2, orientation='v')
+
+if __name__ == "__main__":
+    main()
